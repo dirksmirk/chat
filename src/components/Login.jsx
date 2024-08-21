a752fb65f46a3691b95e893c8eebdffbb5f138d3
Dirk Hubrecht <dirk.holger.hubrecht@gmail.com>
2024-08-21 15:48:31 +0200

    Revert "Start of login page"
    This reverts commit b2cb633c12187dcaea8a505611252c150c91c8f3.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,65 +1,6 @@
-import { useRef } from "react";
 import { FormControl, FormLabel, Button, TextField } from "@mui/material";
 
 const LogIn = () => {
-  const userName = useRef();
-  const password = useRef();
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-
-    if (
-      userName.current.value === "" ||
-      password.current.value === ""
-    ) {
-      alert("Alla fält måste fyllas i.");
-      return; // Stoppa funktionen här om något fält är tomt
-    }
-
-    const data = {
-      username: userName.current.value,
-      password: password.current.value,
-    };
-    console.log(data)
-    try {
-      const response = await fetch(
-        "https://chatify-api.up.railway.app/users",
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(data),
-        }
-      );
-
-      // Parse the response JSON
-      const result = await response.json();
-      console.log(result)
-
-      if (response.ok) {
-        // Handle successful registration
-        console.log("Registration successful:", result);
-      } else {
-        // Handle error response
-        if (result.error === "Username or email already exists") {
-          // Alert the user about the existing username
-          alert(
-            "The username or email you entered already exists. Please choose a different one."
-          );
-        }
-        else {
-          // Handle other possible errors
-          console.error("Error:", result.message);
-          alert("An error occurred: " + result.message);
-        }
-      }
-    } catch (error) {
-      // Handle network or other unexpected errors
-      console.error("Unexpected error:", error);
-      alert("An unexpected error occurred. Please try again later.");
-    }
-  }
 
     return (
         <FormControl>
