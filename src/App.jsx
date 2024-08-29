3b08b96ab0677f329fa0966be9149ca4e48a01ac
Dirk Hubrecht <dirk.holger.hubrecht@gmail.com>
2024-08-29 10:56:50 +0200

    sidenav start

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from './components/Header.jsx';
+import SideNav from './components/SideNav.jsx';
 import Switch from './components/Switch.jsx';
 import SearchContextProvider from './Context';
 import { CustomThemeProvider } from './ThemeContext.jsx';
@@ -8,6 +9,7 @@ function App() {
   return (
     <CustomThemeProvider>
       <SearchContextProvider>
+        <SideNav />
         <Header />
         <Switch />
       </SearchContextProvider>
